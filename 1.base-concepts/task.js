904da54e20417fdb8b7f1c0d7902ea73372f55bc
Gortenzia <tacumineka@gmail.com>
2021-08-02 15:45:53 +0300

    Исправление кода task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,20 @@
+use strict;
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  d = Math.pow(b, 2) - 4 * a * c;
+  if (D < 0) {
+    arr === undefined;
+  } if else (d === 0) {
+    arr.push(-b / (2 * a));
+  } if else (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2a)),
+    arr.push((-b - Math.sqrt(d)) / (2a))
+  };
+  return arr;
 }
+
+solveEquation(2, 3, 4);
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
