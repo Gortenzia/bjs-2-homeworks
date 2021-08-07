aba4aeb43cbce9d56f5e13837f9951ba11f5405c
Gortenzia <tacumineka@gmail.com>
2021-08-07 12:33:02 +0300

    task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,50 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = arr[0];
+  let max = arr[-1];
+  let sum = //сумма всех элементов массива;
+    let avg = Number((sum / arr.length).toFixed(2));
+  for (i = 0, i < arr.length; i++) {
 
-  // Ваш код
+  }
+  function getNumbersFromArr(...arr) {
 
-  return { min:min, max:max, avg:avg };
-}
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    let avg = Number((sum / arr.length).toFixed(2));
 
-// Задание 2
-function worker(arr) {
-  let sum;
+    for (i = 0; i < arr.length; i++) {
+      let result = arr[i];
+      max = max < i ? max = i : max;
+      min = min < i ? min = i : min;
+      sum += result;
+    }
+    return { min: min, max: max, avg: avg };
+  }
 
-  // Ваш код
 
-  return sum;
-}
 
-function makeWork(arrOfArr, func) {
-  let max
+  // Задание 2
+  function worker(arr) {
+    let sum;
 
-  // Ваш кода
-  // for ...
-  
-  return max
-}
+    // Ваш код
 
-// Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+    return sum;
+  }
+
+  function makeWork(arrOfArr, func) {
+    let max
+
+    // Ваш кода
+    // for ...
+
+    return max
+  }
+
+  // Задание 3
+  function worker2(arr) {
+    // Ваш код
+  }
+
