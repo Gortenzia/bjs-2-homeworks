ded036ed54baf0ddd50d8f131412e70693cc7099
Gortenzia <tacumineka@gmail.com>
2021-08-08 15:19:54 +0300

    Homework 2, tets fail 2 of 9

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,50 +1,56 @@
 // Задание 1
-function getArrayParams(arr) {
-  let min = arr[0];
-  let max = arr[-1];
-  let sum = //сумма всех элементов массива;
-    let avg = Number((sum / arr.length).toFixed(2));
-  for (i = 0, i < arr.length; i++) {
+
+function getArrayParams(arr, ...rest) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let result = arr[i];
+    max = max < result ? result : max;
+    min = min > result ? result : min;
+    sum += result;
 
   }
-  function getNumbersFromArr(...arr) {
+  return { max: max, min: min, avg: Number((sum / arr.length).toFixed(2)) };
+}
 
-    let min = Infinity;
-    let max = -Infinity;
-    let sum = 0;
-    let avg = Number((sum / arr.length).toFixed(2));
 
-    for (i = 0; i < arr.length; i++) {
-      let result = arr[i];
-      max = max < i ? max = i : max;
-      min = min < i ? min = i : min;
-      sum += result;
+
+// Задание 2
+function worker(arr, ...rest) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let result = arr[i];
+    sum += result;
+  }
+  return sum;
+}
+
+function makeWork(arrOfArr, func, ...rest) {
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (func(arrOfArr[i]) > max) {
+        return max = func(arrOfArr[i]);
+      }
     }
-    return { min: min, max: max, avg: avg };
   }
+};
 
+makeWork(arrOfArr, worker(arrOfArr));
 
-
-  // Задание 2
-  function worker(arr) {
-    let sum;
-
-    // Ваш код
-
-    return sum;
+// Задание 3
+function worker2(arr, ...rest) {
+  let max = -Infinity;
+  let min = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
   }
-
-  function makeWork(arrOfArr, func) {
-    let max
-
-    // Ваш кода
-    // for ...
-
-    return max
-  }
-
-  // Задание 3
-  function worker2(arr) {
-    // Ваш код
-  }
-
+  return max - min;
+}
