6cde519c2e4f739f2abcce53dfc360895b630b49
Gortenzia <tacumineka@gmail.com>
2021-08-09 21:43:22 +0300

    Edit task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,10 @@
-function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let result1 = arr.filter(item => item > 0)
+  let result2 = result1.filter(item => item % 3 === 0)
+  let resultArr = result2.map((item) => {
+    return item * 10;
+  });
+  return resultArr;
 }
