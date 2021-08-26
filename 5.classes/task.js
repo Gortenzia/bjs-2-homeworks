3f0c1fb011fe3583e9be776e6644d454d0efa44b
Gortenzia <tacumineka@gmail.com>
2021-08-26 17:16:47 +0300

    Не работают findBookBy(type, value) и giveBookByName(bookName)

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -5,8 +5,18 @@ class PrintEditionItem {
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
-        this.type = type;
         this.state = state;
+        this.type = type;
+    }
+
+    get state() {
+        return this.state = this._state;
+    }
+    set state(value) {
+        this.value = value;
+        if (value < 0) this._state = 0;
+        if (value > 100) this._state = 100;
+        else this._state = value;
     }
 
     fix() {
@@ -14,18 +24,9 @@ class PrintEditionItem {
         return this.state;
     }
 
-    set setState(newState) {
-        this.newState = newState;
-        if (newState < 0) this.state = 0;
-        if (newState > 100) this.state = 100;
-        else this.state = this.newState;
-    }
-
-    get setState() {
-        this.state = this.newState;
-    }
-
 }
+
+
 
 class Magazine extends PrintEditionItem {
     constructor(name, releaseDate, pagesCount, state = 100, type = null) {
@@ -72,6 +73,35 @@ class DetectiveBook extends Book {
 }
 
 //Задание 2
+class Library {
 
+    constructor(name = ' ', books) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        this.book = book;
+        if (this.book.state > 30) this.books.push(this.book);
+    }
+
+    findBookBy(type, value) {
+        this.type = type;
+        this.value = value;
+        for (let key in books) {
+            this.keys = type;
+            if (key === value) return this.book;
+            else return null;
+        }
+    }
+
+    giveBookByName(bookName) {
+        this.bookName = bookName;
+        if (bookName < this.books.length) this.books.slice(bookName, 1);
+        else return null;
+    }
+
+}
 
 //Задание 3
+//in process
