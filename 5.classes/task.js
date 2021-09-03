872427e33f2ef2d25d9802495d4761cec30e9e83
Gortenzia <tacumineka@gmail.com>
2021-09-03 17:12:40 +0300

    Попытка сделать 3 задание

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -101,4 +101,88 @@ class Library {
 }
 
 //Задание 3
-//in process
+
+function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+}
+marks = {};
+
+Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark, discipline) {
+    if (mark > 5 || mark < 1) {
+        throw Error('Введите оценку в диапазоне от 1 до 5')
+    } else {
+        if (this.marks[discipline] !== undefined) {
+            this.marks[discipline].push(mark);
+        } else {
+            this.marks[discipline] = [];
+            this.marks[discipline].push(mark);
+        }
+    }
+}
+
+
+Student.prototype.addMarks = function (discipline, ...marks) {
+    if (this.marks[discipline] === undefined) {
+        this.marks[discipline] = [];
+    }
+
+    this.marks[discipline].push(...marks);
+}
+
+Student.prototype.getAverageBySubject = function (discipline) {
+    if (this.marks[discipline] !== undefined) {
+        let sum = 0;
+        for (let mark of this.marks[discipline]) {
+            sum += mark;
+        }
+        return sum / this.marks[discipline].length;
+    }
+}
+
+
+
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    for (let mark of this.marks) {
+        sum += mark;
+    }
+    return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
+    delete this.subjectName;
+    delete this.marks;
+}
+
+
+
+let student1 = new Student('Kolin', 'male', '28');
+let student2 = new Student('Andra', 'male', '18');
+let student3 = new Student('Anna', 'female', '17');
+let student4 = new Student('Tally', 'female', '24');
+let student5 = new Student('Spencer', 'male', '23');
+
+
+const student = new Student("Олег Никифоров");
+student.addMark(5, "algebra");
+student.addMark(5, "algebra");
+student.addMark(5, "geometry");
+student.addMark(4, "geometry");
+try {
+    student.addMark(6, "geometry");
+} catch (err) {
+    console.log('Ошибка! ' + err)
+} // "Ошибка, оценка должна быть числом от 1 до 5"
+student.getAverageBySubject("geometry"); // Средний балл по предмету geometry 4.5
+student.getAverageBySubject("biology"); // Несуществующий предмет
+student.getAverage(); // Средний балл по всем предметам 4.75
+student.exclude("Исключен за попытку подделать оценки");
+
+
